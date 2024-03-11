5c9d1090625286ca0ee053cfbfa94e40dc47b52f
halavathchandulal <halavathchandu2000@gmail.com>
2024-03-11 05:44:38 +0000

    Autonomize AI Products

---
+++ src/App.js
@@ -0,0 +1,18 @@
+// Install Axios if you haven't already: npm install axios
+
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import ProductDetails from './components/ProductDetails'
+import ProductsGrid from './components/ProductsGrid'
+import Home from './components/Home'
+
+const App = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path='/products/:productId' element={<ProductDetails />} />
+      <Route path='/products' element={<ProductsGrid />} />
+      <Route path='/' element={<Home />} />
+    </Routes>
+  </BrowserRouter>
+)
+
+export default App
